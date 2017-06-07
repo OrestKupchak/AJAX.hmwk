b0157e3b2cd367c5cb6249c6c13bc3d78c909882
Elvis <orest.kupchak@gmail.com>
2017-06-07 17:56:25 +0300

    Add files via upload

--- location_weather/js/app.js
+++ location_weather/js/app.js
@@ -1,11 +1,77 @@
 
-$.ajax({
-         url: 'https://www.where-am-i.net/',
-         type: "GET",
-         beforeSend: function(xhr){xhr.setRequestHeader('Origin', 'http://127.0.0.1:8080/');},
-         success: function() { alert('Success!'); }
-      });
+var outputElem = document.body
+//outputElem.innerText = 'Хз чи воно працює'
 
+function coordinates(){
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://darksky.net/details/49.8454,24.0054/' +  date.getFullYear() +'-'+ date.getMonth()+'-' + date.getDate() + '/si24/en')
+  //xhr.open('GET', 'https://darksky.net/forecast/')
+
+  xhr.onreadystatechange = function(){
+   if (xhr.readyState !=4) return;
+    if (xhr.status != 200) {
+      alert('Error '+ xhr.status + ':' +xhr.statusText)
+      return;
+  console.log(xhr.response)
+   }
+    console.log(xhr);
+    outputElem.innerHTML = xhr;
+  }
+  outputElem.innerText = xhr;
+  xhr.send();
+}
+
+
+
+var previous = document.getElementById('previous')
+previous.addEventListener('click', getPrevWeather)
+
+function getPrevWeather() {
+    var date = new Date;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://darksky.net/details/49.8454,24.0054/' +  date.getFullYear() +'-'+ date.getMonth()+'-' + date.getDate()-1 + '/si24/en')
+
+    xhr.onreadystatechange = function(){
+     if (xhr.readyState !=4) return;
+      if (xhr.status != 200) {
+        alert('Error '+ xhr.status + ':' +xhr.statusText)
+        return;
+          console.log(xhr.response)
+     }
+      console.log(xhr);
+      outputElem.innerHTML = xhr;
+    }
+    outputElem.innerText = xhr;
+    xhr.send();
+    }
+
+
+
+  //  var next = document.getElementById('next')
+  //  next.addEventListener('click', getNextWeather)
+
+    function getNextWeather(e) {
+        var date = new Date;
+        var xhr = new XMLHttpRequest();
+        var default = 'https://darksky.net/details/49.8454,24.0054/' +  date.getFullYear() +'-'+ date.getMonth()+'-' + date.getDate() + '/si24/en'
+        if ()
+
+        xhr.open('GET', 'https://darksky.net/details/49.8454,24.0054/' +  date.getFullYear() +'-'+ date.getMonth()+'-' + date.getDate()+1 + '/si24/en')
+
+        xhr.onreadystatechange = function(){
+         if (xhr.readyState !=4) return;
+          if (xhr.status != 200) {
+            alert('Error '+ xhr.status + ':' +xhr.statusText)
+            return;
+              console.log(xhr.response)
+         }
+          console.log(xhr);
+          outputElem.innerHTML = xhr;
+        }
+        outputElem.innerText = xhr;
+        xhr.send();
+        }
 
 
 
@@ -18,12 +84,12 @@ function coordinates(){
   xhr.open('GET', 'https://www.where-am-i.net/')
   xhr.setRequestHeader('Origin', 'http://127.0.0.1:8080/');
   xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-  xhr.setRequestHeader('Access-Control-Allow-Credentials', true);
+  xhr.setRequestHeader('Access-Control-Allow-Credentials', 'true');
   xhr.setRequestHeader ('Access-Control-Allow-Headers', 'Content-Length')
   xhr.setRequestHeader ('Access-Control-Expose-Headers', 'Content-Length');
 
   xhr.timeout = 0
-  xhr.onreadyststechange = function(){
+  xhr.onreadystatechange = function(){
    if (xhr.readyState !=4) return;
     if (xhr.status != 200) {
       alert('Error '+ xhr.status + ':' +xhr.statusText)
@@ -35,4 +101,5 @@ function coordinates(){
   outputElem.innerHTML = xhr.responseText
   xhr.send();
 }
+
 */
