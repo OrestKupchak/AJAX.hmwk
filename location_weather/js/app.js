20f88104186691c93732896cb3cd223c01df0b8c
Orest Kupchak <Orest_Kupchak@epam.com>
2017-06-13 17:50:09 +0300

    add "getPrevWeather()" test version

--- location_weather/js/app.js
+++ location_weather/js/app.js
@@ -28,37 +28,61 @@ window.onload = function coordinates(){
 
 }
 
-
+//var myDate = new Date( data.daily.data[0].time *1000);
+//document.write(myDate.toGMTString()+"<br>"+myDate.toLocaleString());
 //var outputElem = document.getElementById('content')
 
-jQuery(document).ready(function() { 
-
+jQuery(document).ready(function() {
   var proxy = 'https://cors-anywhere.herokuapp.com/';
-  var apiLinkDS = "https://api.darksky.net/forecast/d407554e29ad2fe7013dc4a60a1d1682/49.839683,24.029717";
+  var apiLinkDS = "https://api.darksky.net/forecast/d407554e29ad2fe7013dc4a60a1d1682/49.8454,24.0054,1497360590";
 
   jQuery.ajax({
     url: proxy + apiLinkDS,
-    success:function(data) { 
+    success:function(data) {
       console.log(data);
 
+      var time = data.currently.time
+      var myDate = new Date(time *1000);
+      console.log('current time', myDate.toGMTString()+"<br>"+myDate.toLocaleString());
+      apiLinkDS += time
+        document.getElementById('date').innerText = myDate.toGMTString()
+        document.getElementById('summary').innerText = data.currently.summary
+        document.getElementById('temperature').innerText = 'Max temperature: ' + data.currently.apparentTemperatureMax
+        document.getElementById('wind').innerText = 'Wind speed: '+ data.currently.windSpeed + ' m/s';
+        document.getElementById('pressure').innerText = 'Pressure:' +data.currently.pressure
 
-        document.getElementById('summary').innerText = data.daily.summary 
-        document.getElementById('temperature').innerText = 'Max temperature: ' + data.daily.data[4].apparentTemperatureMax
-        document.getElementById('wind').innerText = 'Wind speed: '+ data.daily.data[4].windSpeed + ' m/s';
-        document.getElementById('pressure').innerText = 'Pressure:' +data.daily.data[4].pressure  
-
-        if (data.daily.icon === 'rain') {
+        if (data.currently.icon === 'rain') {
           document.body.style.backgroundImage = "url('rain.jpg')"
-
-        }
+      }
     }
   });
-
-
-
 });
 
 
+function getPrevWeather() {
+  var proxy = 'https://cors-anywhere.herokuapp.com/';
+  var apiLinkDS = "https://api.darksky.net/forecast/d407554e29ad2fe7013dc4a60a1d1682/49.8454,24.0054,1497274190";
+
+  jQuery.ajax({
+    url: proxy + apiLinkDS,
+    success:function(data) {
+      console.log(data);
+
+      var time = data.currently.time - 86400
+      var myDate = new Date(time * 1000);
+      console.log('prev time', myDate.toGMTString()+"<br>"+myDate.toLocaleString());
+  //apiLinkDS += time
+        document.getElementById('summary').innerText = data.currently.summary
+        document.getElementById('temperature').innerText = 'Max temperature: ' + data.currently.apparentTemperatureMax
+        document.getElementById('wind').innerText = 'Wind speed: '+ data.currently.windSpeed + ' m/s';
+        document.getElementById('pressure').innerText = 'Pressure:' +data.currently.pressure
+
+        if (data.currently.icon === 'rain') {
+          document.body.style.backgroundImage = "url('rain.jpg')"
+      }
+    }
+  })
+}
 /*
  function getWeather(coords){
   var date = new Date;
